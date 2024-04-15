c3b3aa4e97955f089b2ab73fc3968247a7b31645
Michael Aggerholm <92aggerholm@gmail.com>
2024-04-15 23:53:01 +0200

    tilføjet auth routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
@@ -7,6 +8,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PagesController::class, 'home'])->name('home');
 Route::get('/book/{id}', [PagesController::class, 'book'])->name('book');
 Route::get('/account', [PagesController::class, 'account'])->name('account')->middleware('auth');
+
+// Auth
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login')->middleware('guest');
+Route::post('/login', [AuthController::class, 'postLogin'])->middleware('guest');
+
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register')->middleware('guest');
+Route::post('/register', [AuthController::class, 'postRegister'])->middleware('guest');
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
+
 
 // Route for clearing caches
 Route::get('/clear-caches', function () {
