206e359f0c1038efe7c54997677b212799270970
Michael Aggerholm <92aggerholm@gmail.com>
2024-04-15 12:01:28 +0200

    tilføjet routes til clear cache of link storage for let tilgang via browser.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+// Route for clearing caches
+Route::get('/clear-caches', function () {
+    Artisan::call('route:cache');
+    Artisan::call('config:cache');
+    Artisan::call('cache:clear');
+    Artisan::call('view:clear');
+    return 'clear';
+});
+
+// Route for linking storage
+Route::get('/link-storage', function () {
+    Artisan::call('storage:link');
 });
