309674f3781bd441fc260163963e742aba6a6abe
Michael Aggerholm <92aggerholm@gmail.com>
2024-04-15 07:50:48 +0200

    oprettet og udfyldt migration for book_genre og book_author som mange-til-mange relation

---
+++ database/migrations/2024_04_15_053919_create_book_genre_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('book_genre', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->foreignId('book_id')->constrained()->onDelete('cascade');
+            $table->foreignId('genre_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
