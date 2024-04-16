916a4fdf5fd8af45d581d58f0261900da0d421e3
Michael Aggerholm <92aggerholm@gmail.com>
2024-04-16 10:13:04 +0200

    Lavet korrekt funktionalitet for admin middleware som nu skal styre adminpanel adgang

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class Admin
@@ -15,6 +16,12 @@ class Admin
      */
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request);
+        if (Auth::user()) {
+            if (Auth::user()->is_admin) {
+                return $next($request);
+            }
+            return redirect('/');
+        }
+        return redirect('/login');
     }
 }
