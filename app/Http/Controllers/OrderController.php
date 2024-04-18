cc006914f47ee3b81c98d25e4c8a5be069986e94
Michael Aggerholm <92aggerholm@gmail.com>
2024-04-18 21:22:25 +0200

    Tilføjet ordre og item controller samt model for håndtering af ordrer og items til databasen samt funktionalitet for a gemme ordrer.

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\order;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function index() {
+        $orders = Order::with('user')->orderBy('created_at', 'desc')->get();
+
+        return view('admin.pages.orders.index', ['orders' => $orders]);
+    }
+
+    public function view($id) {
+        $states = ['pending', 'processing', 'shipped', 'cancelled'];
+        $order = Order::with('user', 'items', 'items.book')->findOrFail($id);
+
+        return view('admin.pages.orders.view', ['order' => $order, 'states' => $states]);
+    }
+
+    public function updateStatus($id, Request $request) {
+        $order = Order::findOrFail($id);
+        $order->status = $request->status;
+        $order->save();
+
+        return redirect()->back()->with('success', 'Ordre status opdateret!');
+    }
+}
---
+++ app/Models/Item.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function book() {
+        return $this->belongsTo(Book::class);
+    }
+
+    public function order() {
+        return $this->belongsTo(Order::class);
+    }
+}
