3cad9d345b2a465a2dcc60579ab5403cd175ff8b
Michael Aggerholm <92aggerholm@gmail.com>
2024-04-18 22:14:37 +0200

    rettelse af larastan fejl fund.

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -11,11 +11,13 @@ class Order extends Model
 
     protected $guarded = [];
 
-    public function user() {
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
         return $this->belongsTo(User::class);
     }
 
-    public function items() {
+    public function items(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
         return $this->hasMany(Item::class);
     }
 }
