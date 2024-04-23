b0a0894942ab9547840656ac17e5baac3ead94d8
Michael Aggerholm <92aggerholm@gmail.com>
2024-04-23 15:00:18 +0200

    dashboard med view over antal af diverse elementer på siden.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,12 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Author;
+use App\Models\Book;
+use App\Models\Condition;
+use App\Models\Format;
+use App\Models\Genre;
+use App\Models\Language;
+use App\Models\Publisher;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    public function dashboard()
-    {
-        return view('admin.pages.dashboard');
+    public function dashboard() {
+        $authorCount = Author::count();
+        $bookCount = Book::count();
+        $userCount = User::count();
+        $conditionCount = Condition::count();
+        $formatCount = Format::count();
+        $genreCount = Genre::count();
+        $languageCount = Language::count();
+        $publisherCount = Publisher::count();
+
+        return view('admin.pages.dashboard', [
+            'authorCount' => $authorCount,
+            'bookCount' => $bookCount,
+            'userCount' => $userCount,
+            'conditionCount' => $conditionCount,
+            'formatCount' => $formatCount,
+            'genreCount' => $genreCount,
+            'languageCount' => $languageCount,
+            'publisherCount' => $publisherCount,
+        ]);
     }
 }
--- resources/views/admin/pages/dashboard.blade.php
+++ resources/views/admin/pages/dashboard.blade.php
@@ -2,5 +2,14 @@
 @section('title', 'Dashboard')
 @section('content')
     <h1 class="page-title">Dashboard</h1>
-    <p>Her er intet..</p>
+    <p>Antal bøger: {{$bookCount}}</p>
+    <p>Antal forfattere: {{$authorCount}}</p>
+    <p>Antal brugere: {{$userCount}}</p>
+    <p>Antal tilstande: {{$conditionCount}}</p>
+    <p>Antal formater: {{$formatCount}}</p>
+    <p>Antal genrer: {{$genreCount}}</p>
+    <p>Antal sprog: {{$languageCount}}</p>
+    <p>Antal udgivere: {{$publisherCount}}</p>
+    <br>
+    <p>Ellers intet at vise..</p>
 @endsection
